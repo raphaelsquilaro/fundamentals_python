3cb095ab42fc4c88f13f094dbc09f4665ed6f259
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-03-26 19:19:09 -0300

    enviando os primeiros arquivos

---
+++ fp_aula03_ex01.py
@@ -0,0 +1,9 @@
+# Author: Raphael Campos Squilaro
+# Project: Condicional Struture with celsius
+
+celsius = float(input("Digite a temperatura em Celsius: "))
+
+if celsius <= 20:
+    print("Está frio!")
+else:
+    print("Está Quente!")
