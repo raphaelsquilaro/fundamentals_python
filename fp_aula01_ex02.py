425f92b0ba6b66eb1ed71cd73e44723650126d55
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:48:45 -0300

    Add basic arithmetic operations in Python script

---
+++ fp_aula01_ex02.py
@@ -0,0 +1,15 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - My First Python Project
+
+valor1 = float(input('Digite o primeiro valor: '))
+valor2 = float(input('Digite o segundo valor: '))
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+print(f'O resultado da soma é: {soma}')
+print(f'O resultado da subtração é: {subtracao}')   
+print(f'O resultado da multiplicação é: {multiplicacao}')
+print(f'O resultado da divisão é: {divisao}')
