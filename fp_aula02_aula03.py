b7999b75e863e73ce7b1ddd63d7c7092ef443103
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:51:45 -0300

    Create fp_aula02_aula03.py with print examples
    Add string manipulation and formatting examples.

---
+++ fp_aula02_aula03.py
@@ -0,0 +1,22 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - String Manipulation and Formatting Demonstration
+
+# \n -> nova linha
+print("Olá, mundo!")
+print()
+print('Meu nome é \nNilton Barros')
+
+# end - sep
+# nome do banco | identificação do cliente | valor da transação
+# idtransacao_bradesco_niltonbarros_100045
+print('12b27ask', 'bradesco', 'niltonbarro', '100045', sep='_', end='.pdf')
+
+
+print()
+
+# desenho
+print("""
+ /\\_/\\
+( o.o )
+ > ^ <
+""")
