43b530624ba36a23fddd6eef83ebcd93b016ea67
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:48:28 -0300

    Add initial Python program for basic calculator

---
+++ fp_aula01_ex01.py
@@ -0,0 +1,17 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - My First Python Project
+
+print('Olá Mundo!')
+print('Meu primeiro programa em Python')
+print('Raphael Campos Squilaro')
+
+# Criação de variáveis para uma calculadora de somar
+# regras básicas para criar variáveis:
+# nunca usar acentos, espaços ou caracteres especiais
+# o nome da variável deve ser autoexplicativo
+# nunca usar palavras reservadas
+
+valor1 = 114
+valor2 = 30
+soma = valor1 + valor2
+print(soma)
