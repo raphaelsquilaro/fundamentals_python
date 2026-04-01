a07bd868b14a1f7ef2caecdfe8f30dac6b266dae
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 19:54:01 -0300

    Create fp_aula05_ex01.py for loop examples
    Add a Python script demonstrating for loops and range usage.

---
+++ fp_aula05_ex01.py
@@ -0,0 +1,12 @@
+# Author: Raphael Campos Squilaro
+# Project Name: testing and learn about loop with for
+
+# in - is a keyword used to check if a value exists in a sequence (list, tuple, string) or to iterate over a sequence of values.
+# range - is a built-in function that generates a sequence of numbers, which can be used in for loops to iterate a specific number of times.
+# The for loop is a control flow statement that allows you to iterate over a sequence of values, executing a block of code for each value in the sequence.
+
+for numero in range(1, 6):
+    print(f"Contagem: {numero}")
+
+for asterisco in range(1, 6):
+    print('*' * asterisco)
