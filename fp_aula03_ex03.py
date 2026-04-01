77b3708dcae87e3fa66f054d07737a011aa66808
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:53:30 -0300

    Add user access check in fp_aula03_ex03.py

---
+++ fp_aula03_ex03.py
@@ -0,0 +1,10 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - Program to Calculate Student's Average Grade and Determine Pass/Fail Status
+
+usuario = 'raphael'
+acesso = input('Digite o usuario: ')
+
+if acesso == usuario:
+    print('Acesso permitido!')
+else:
+    print('Acesso negado!')
