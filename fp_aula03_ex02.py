9776757bd98a40e735a20a2d9503ed0065072677
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:53:12 -0300

    Add student average grade calculation program
    Implement student average grade calculation and pass/fail status.

---
+++ fp_aula03_ex02.py
@@ -0,0 +1,15 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - Program to Calculate Student's Average Grade and Determine Pass/Fail Status
+
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+nota3 = float(input('Digite a terceira nota do aluno: '))
+nota4 = float(input('Digite a quarta nota do aluno: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if media < 5:
+    print(f'Aluno reprovado!:( {media:.2f}')
+elif media < 7:
+    print(f'Aluno em recuperação! {media:.2f}') 
+else:
+    print(f'Aluno aprovado! {media:.2f}')
