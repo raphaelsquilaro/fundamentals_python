56a350ef263eeb422d44fb2419b5a183ac860bc1
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:51:28 -0300

    Add f-strings and round function examples
    Demonstrate f-strings and round function with PI.

---
+++ fp_aula02_aula02.py
@@ -0,0 +1,15 @@
+# Author: Raphael Campos Squilaro
+# Project Name: FP - f-strings and round function demonstration
+
+# Constantes ficam em letras maiúsculas
+PI = 3.14159265
+
+# utilizando o f-strings
+print(f"O valor de pi é: {PI:.2f}")
+print(f"O valor de pi é: {PI:.3f}")
+print(f"O valor de pi é: {PI:.4f}")
+print(40*'-')
+# utilizando a função round()
+print(f"O valor de pi é: {round(PI, 2)}")
+print(f"O valor de pi é: {round(PI, 3)}")
+print(f"O valor de pi é: {round(PI, 4)}")
