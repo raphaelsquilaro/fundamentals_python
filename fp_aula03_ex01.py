96b317ecb7cace59239bb3a62e4c1f09352758ea
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-01 18:52:52 -0300

    Replace temperature check with student average calculation

--- fp_aula03_ex01.py
+++ fp_aula03_ex01.py
@@ -1,9 +1,13 @@
 # Author: Raphael Campos Squilaro
-# Project: Condicional Struture with celsius
+# Project Name: FP - Program to Calculate Student's Average Grade and Determine Pass/Fail Status
 
-celsius = float(input("Digite a temperatura em Celsius: "))
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+nota3 = float(input('Digite a terceira nota do aluno: '))
+nota4 = float(input('Digite a quarta nota do aluno: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
 
-if celsius <= 20:
-    print("Está frio!")
+if media >= 5:
+    print(f'Aluno aprovado!{media:.2f}')
 else:
-    print("Está Quente!")
+    print(f'Aluno reprovado!:( {media:.2f}')
