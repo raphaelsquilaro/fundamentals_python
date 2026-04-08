dd1af64909eca0e9e6973f7d54ceb39ecbb6480d
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-08 19:11:41 -0300

    Update fp_aula02_desafio03.py

--- fp_aula02_desafio03.py
+++ fp_aula02_desafio03.py
@@ -1,3 +1,5 @@
+#Author: Raphael Campos Squilaro
+
 #3️⃣ Desafio adicional: A hamburgueria LANCHÃO TOP tem 4 motoboys que realizam as entregas aos clientes, cada motoboy recebe um valor padrão para fazer as entregas, quanto mais entregas, mais eles ganham, os valores pagos tem relação com a distância, a empresa paga R$ 1,00 por km para cada motoboy.
 
 distancia_km = float(input('Digite a distância percorrida em km: '))
