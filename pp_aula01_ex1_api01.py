12de9c0ed4c74302e7e5703a9a04885ff4bef73d
raphaelsquilaro <raphael.squilaro@aluno.senai.br>
2026-04-16 21:24:57 -0300

    Update pp_aula01_ex1_api01.py

--- pp_aula01_ex1_api01.py
+++ pp_aula01_ex1_api01.py
@@ -3,15 +3,10 @@
 
 import requests
 
-cep = input("Digite seu cep: ")
-
-url = f"https://viacep.com.br/ws/{cep}/json/"
+url = "https://economia.awesomeapi.com.br/last/USD-BRL"
 resposta = requests.get(url)
 dados = resposta.json()
 
-print()
-print()
-print(f"Logradouro: {dados['logradouro']}")
-print(f"Bairro: {dados['bairro']}")
-print(f"Cidade: {dados['localidade']}")
-print(f"UF: {dados['uf']}")
+valor_dolar = dados['USDBRL']['bid']
+
+print(f"Cotação atual do dólar: {valor_dolar}")
